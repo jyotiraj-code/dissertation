1a28f8a587b45996443c25752beec1ccfa09454a
Jyotiraj Nath <jyotirajnath7@gmail.com>
2024-09-19 12:51:50 +0530

    changes made for tensor network implementation
    working with the new implementation of the paper using tensor networks this time

---
+++ Paper Reading/Paper 1 Modifications/mv.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from pypfopt import risk_models, expected_returns, EfficientFrontier
+import os
+
+class MeanVarianceOptimization:
+    def __init__(self, stocks, start_date, end_date, data_dir="C:\\Users\\jrnat\\Documents\\GitHub\\dissertation-new\\dissertation\\Paper Reading\\Paper 1 Modifications"):
+        self.stocks = stocks
+        self.start_date = start_date
+        self.end_date = end_date
+        self.data_dir = data_dir  # Ensure correct directory is being used
+        self.prices = self.get_stock_data()
+
+    def get_stock_data(self):
+        stock_data = pd.DataFrame()
+
+        for stock in self.stocks:
+            # Adjusting file path for "COMPANY.NS.csv" in correct directory
+            file_path = os.path.join(self.data_dir, f'{stock}.NS.csv')
+            #print(f"Looking for file: {file_path}")  # Debugging print to verify file path
+
+            if os.path.exists(file_path):
+                data = pd.read_csv(file_path, index_col='Date', parse_dates=True)
+                # Use 'Adj Close' for adjusted closing prices
+                data = data['Adj Close'].loc[self.start_date:self.end_date]
+                stock_data[stock] = data
+            else:
+                print(f"Data for {stock} not found in {self.data_dir}")
+
+        return stock_data.dropna(how="all")
+
+    def cov(self):
+        cov = risk_models.CovarianceShrinkage(self.prices).ledoit_wolf()
+        return cov
+
+    def expected_returns(self):
+        mu = expected_returns.capm_return(self.prices)
+        return mu
+
+    def weights(self):
+        er = self.expected_returns()
+        cov = self.cov()
+
+        ef = EfficientFrontier(er, cov, weight_bounds=(None, None))
+        ef.min_volatility()
+        weights = ef.clean_weights()
+        return weights
